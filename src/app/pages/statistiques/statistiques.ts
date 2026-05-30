c5802c603580d6e54aada68919aec1772a8aa8b9
jadone237 <martinfouepi@gmail.com>
2026-05-30 23:56:53 +0100

    3me commit

---
+++ src/app/pages/statistiques/statistiques.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-statistiques',
+  imports: [],
+  templateUrl: './statistiques.html',
+  styleUrl: './statistiques.css',
+})
+export class Statistiques {}
