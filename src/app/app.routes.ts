d0cbccdc177f26d93c4d2381de2c373d4028a2fa
YouyouGlody <Youyoustyve@gmail.com>
2026-06-03 03:09:49 +0100

    configuration routes et correction tsconfig

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { List } from './pages/reservation/list/list';
+import { Form } from './pages/reservation/form/form';
+import { Confirmation } from './pages/reservation/confirmation/confirmation';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'reservations', pathMatch: 'full' },
+  { path: 'reservations', component: List },
+  { path: 'reserver', component: Form },
+  { path: 'confirmation', component: Confirmation },
+];
