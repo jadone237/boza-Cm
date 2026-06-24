84f41c9d8b624e46c6be0353ac87f398e53b801e
jadone237 <martinfouepi@gmail.com>
2026-06-24 10:48:41 +0100

    AJOUTS DE TRAJETS

--- src/app/services/trajet.spec.ts
+++ src/app/services/trajet.spec.ts
@@ -1,16 +1,47 @@
-import { TestBed } from '@angular/core/testing';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-import { Trajet } from './trajet';
+@Injectable({
+  providedIn: 'root'
+})
+export class TrajetService {
 
-describe('Trajet', () => {
-  let service: Trajet;
+  private baseUrl = 'http://localhost:8080/api/v1/trajets';
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(Trajet);
-  });
+  constructor(private http: HttpClient) {}
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+  // CRUD
+  getAllTrajets(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/get_all`);
+  }
+
+  getAllTrajetsPaginated(page: number, size: number, sortBy: string): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/get_all_page?page=${page}&size=${size}&sortBy=${sortBy}`);
+  }
+
+  getTrajetById(id: number): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/get_by_id/${id}`);
+  }
+
+  createTrajet(trajet: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/create`, trajet);
+  }
+
+  updateTrajet(id: number, trajet: any): Observable<any> {
+    return this.http.put(`${this.baseUrl}/update/${id}`, trajet);
+  }
+
+  deleteTrajet(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/delete/${id}`);
+  }
+
+  // Recherches
+  getTrajetsByDepart(depart: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/search/depart/${depart}`);
+  }
+
+  getTrajetsByRoute(depart: string, arrivee: string): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/search/route/${depart}/${arrivee}`);
+  }
+}
